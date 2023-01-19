34fbc50bd1296dcb8925a35dfd9b6cb0b4289f31
Vadim Makeev <hi@pepelsbey.dev>
2023-01-19 18:12:27 +0100

    Init

---
+++ eleventy.config.js
@@ -0,0 +1,58 @@
+const fs = require('node:fs');
+const yaml = require('js-yaml');
+const music = require('music-metadata');
+const htmlmin = require('html-minifier');
+const xml = require('minify-xml');
+
+module.exports = (config) => {
+	config.addDataExtension('yml', (contents) => {
+		return yaml.load(contents);
+	});
+
+	config.addFilter('length', (path) => {
+		const stats = fs.statSync(path);
+
+		return stats.size;
+	});
+
+	const getDuration = async (path) => {
+		const metadata = await music.parseFile(path, { duration: true });
+		const duration = parseFloat(metadata.format.duration);
+		const formatted = new Date(Math.ceil(duration) * 1000)
+			.toISOString()
+			.substring(11, 19);
+
+		return formatted;
+	}
+
+	config.addAsyncFilter('duration', async (path) => {
+		const duration = await getDuration(path);
+		console.log(duration);
+		return duration;
+	});
+
+	config.addFilter('htmlmin', (value) => {
+		return htmlmin.minify(
+			value, {
+				removeComments: true,
+				collapseWhitespace: true,
+			}
+		);
+	});
+
+	// config.addTransform('xmlmin', (content, outputPath) => {
+	// 	if (outputPath && outputPath.endsWith('.xml')) {
+	// 		return xml.minify(content);
+	// 	}
+
+	// 	return content;
+	// });
+
+	return {
+		dir: {
+			input: 'src',
+			output: 'dist',
+			data: 'data'
+		}
+	};
+};
---
+++ src/feed/index.11ty.js
@@ -0,0 +1,57 @@
+exports.data = {
+	permalink: 'index.xml',
+}
+
+exports.render = function(data) {
+	return `
+		<?xml version="1.0" encoding="utf-8"?>
+		<rss
+			xmlns:atom="http://www.w3.org/2005/Atom"
+			xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
+			version="2.0"
+		>
+			<channel>
+
+				<title>${ data.meta.title }</title>
+				<description>${ data.meta.description }</description>
+				<copyright>${ data.meta.copyright }</copyright>
+				<language>${ data.meta.language }</language>
+				<link>${ data.meta.url }</link>
+
+				<atom:link href="${ data.meta.url }feed/" rel="self" type="application/rss+xml"/>
+				<itunes:subtitle>${ data.meta.subtitle }</itunes:subtitle>
+				<itunes:type>${ data.meta.type }</itunes:type>
+				<itunes:author>${
+					data.meta.authors.map(author =>
+						`${ author }`
+					).join(', ')
+				}</itunes:author>
+				<itunes:explicit>${ data.meta.explicit }</itunes:explicit>
+				<itunes:owner>
+					<itunes:name>${ data.meta.owner.name }</itunes:name>
+					<itunes:email>${ data.meta.owner.email }</itunes:email>
+				</itunes:owner>
+				<itunes:image href="${ data.meta.url }cover.png"/>
+				<itunes:category text="${ data.meta.category }">
+					<itunes:category text="${ data.meta.subcategory }"/>
+				</itunes:category>
+			</channel>
+			${data.collections.episode.map(
+				episode => `
+					<item>
+						<title>${ episode.data.title }</title>
+						<link>${ data.meta.url }${ episode.fileSlug }/</link>
+						<pubDate>${ episode.date.toUTCString() }</pubDate>
+						<description><![CDATA[${ this.htmlmin(episode.content) }]]></description>
+						<guid isPermaLink="true">${ data.meta.url }episodes/${ episode.fileSlug }.mp3</guid>
+						<enclosure
+							type="audio/mpeg"
+							url="${ data.meta.url }episodes/${ episode.fileSlug }.mp3"
+							length="${ this.duration(`src/mp3/${ episode.fileSlug }.mp3`) }"
+						/>
+					</item>
+				`
+			).join('')}
+		</rss>
+	`;
+};
